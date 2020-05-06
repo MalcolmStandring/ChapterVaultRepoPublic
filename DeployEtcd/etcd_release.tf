90f248884b5910b12bb378b55c54e0f193863dea
Malcolm Standring <malcolm.standring@gmail.com>
2020-05-06 11:53:46 +0100

    Working etcd operator

--- DeployEtcd/etcd_release.tf
+++ DeployEtcd/etcd_release.tf
@@ -2,11 +2,11 @@ data "helm_repository" "stable" {
   name = "stable"
   url  = "https://kubernetes-charts.storage.googleapis.com"
 }
-resource "helm_release" "etcd_operator" {
-  name       = "etcd_operator"
-  repository = data.helm_repository.stable.metadata[0].name
+resource "helm_release" "etcd-operator" {
+  name       = "etcd-operator"
+  repository = data.helm_repository.stable.metadata[0].url
   chart      = "etcd-operator"
-  version    = "0.9.4"
+  # version    = "3.4.0"
 
   values = [
     "${file("etcd_operator_values.yaml")}"
