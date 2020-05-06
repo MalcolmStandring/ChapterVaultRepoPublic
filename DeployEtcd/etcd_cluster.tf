ce7999f5fcab7c708349c730a83d7c3d5d0036f4
Malcolm Standring <malcolm.standring@gmail.com>
2020-05-06 14:53:17 +0100

    ackout to working

--- DeployEtcd/etcd_cluster.tf
+++ DeployEtcd/etcd_cluster.tf
@@ -1,12 +1,12 @@
 # Stands up the actual Etcd cluster nodes using the Operator
-resource "kubernetes" "EtcdCluster" {
-    metadata {
-        name = "etcd-cluster-vault"
-        labels = {
-            App = "Etcd"
-        }
-    }
-    spec {
-        size = 1
-    }
-}
+#resource "kubernetes" "EtcdCluster" {
+#    metadata {
+#       name = "etcd-cluster-vault"
+#        labels = {
+#           App = "Etcd"
+#        }
+#    }
+#    spec {
+#        size = 1
+#    }
+#}
