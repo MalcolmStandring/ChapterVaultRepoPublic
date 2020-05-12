dbc6d7dc336cb6fa951ad88345098cb7988e7d63
Malcolm Standring <malcolm.standring@gmail.com>
2020-05-12 08:13:24 +0100

    Tidied up file system, ready to merge

--- DeployVault/provider.tf
+++
@@ -1,4 +0,0 @@
-# This provider file assumes we will connect to a local Kubernetes cluster,
-# pointed to by "kubectl", so no need to specify connection information.
-# This may be a local instance of minikube, running under Hyper-V.
-provider "kubernetes" {}
