6fc7be5f26b7415de06ae5f4686b2d155c26ffc4
Malcolm Standring <malcolm.standring@gmail.com>
2020-05-13 18:16:15 +0100

    Commit of pre-merge "provider.tf"

--- ConfigureVault/provider.tf
+++ ConfigureVault/provider.tf
@@ -1,11 +1,4 @@
 provider "vault" {
-# It is strongly recommended to configure this provider through the
-# environment variables described above, so that each user can have
-# separate credentials set in the environment.
-#
-# This will default to using $VAULT_ADDR
-# But can be set explicitly
-  address = "http://localhost:8200"
-#  token = "s.zwdb7y69ch3K60Y8ji9w1Oes"
-  skip_tls_verify = true
+    address="http://vault-vault.apps.cluster-1558.sandbox1776.opentlc.com:80"
+    token = "s.S6XHSSiBvrgiZ7FmeVifjRSY"
 }
