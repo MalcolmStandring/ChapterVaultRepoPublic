414ca9c6f45be609166b2d9bb455b83f0e94b2ee
Bajwa <ali.bajwa@capgemini.com>
2020-05-13 14:31:29 +0100

    updated .tf files

--- ConfigureVault/groups.tf
+++ ConfigureVault/groups.tf
@@ -1,12 +1,13 @@
 resource "vault_identity_group" "admin_group" {
   name = "administrators"
-  type = "internal"
   policies = [vault_policy.admin_policy.name]
 }
 
 resource "vault_identity_group" "provisioner_group" {
   name = "provisioners"
-  type = "internal"
   policies = [vault_policy.provisioner_policy.name]
 }
-
+resource "vault_identity_group" "developer_group" {
+  name = "developers"
+  policies = [vault_policy.developer_policy.name]
+}
--- ConfigureVault/policies.tf
+++ ConfigureVault/policies.tf
@@ -7,3 +7,14 @@ resource "vault_policy" "provisioner_policy" {
   name   = "provisioners"
   policy = file("policies/provisioner-policy.hcl")
 }
+
+resource "vault_policy" "operations_policy" {
+  name = "operations"
+  policy = file("policies/operations_policy.hcl")
+}
+
+resource "vault_policy" "developer_policy" {
+  name = "developers"
+  policy = file("policies/developer_policy.hcl")
+}
+
